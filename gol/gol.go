3fcc26e7b7a6796c5a3794054ecc323f4dce1ffa
zzq <zzq_0616@outlook.com>
2025-10-02 12:05:11 +0100

    gol method 2

--- gol/gol.go
+++ gol/gol.go
@@ -1,73 +1,75 @@
 package main
 
 func calculateNextState(p golParams, world [][]byte) [][]byte {
-
 	h, w := p.imageHeight, p.imageWidth
+	aliveVal := byte(255) // 活细胞是 255；死细胞是 0
 
-	aliveVal := byte(255)
-	deadVal := byte(0)
-
-	// 生成下一代
+	// 准备下一代
 	next := make([][]byte, h)
 	for y := range next {
 		next[y] = make([]byte, w)
 	}
 
-	// 8 邻域偏移
-	neis := [8][2]int{
-		{-1, -1}, {-1, 0}, {-1, 1},
-		{0, -1}, {0, 1},
-		{1, -1}, {1, 0}, {1, 1},
-	}
-
 	for y := 0; y < h; y++ {
 		for x := 0; x < w; x++ {
-			aliveNeighbours := 0
-			for _, d := range neis {
-				ny := (y + d[0] + h) % h
-				nx := (x + d[1] + w) % w
-				if world[ny][nx] == aliveVal { // 非 0 视为活
-					aliveNeighbours++
-				}
+			sum := 0
+			if world[(y+h-1)%h][(x+w-1)%w] != 0 {
+				sum++
+			}
+			if world[(y+h-1)%h][(x+w)%w] != 0 {
+				sum++
+			}
+			if world[(y+h-1)%h][(x+w+1)%w] != 0 {
+				sum++
+			}
+			if world[(y+0)%h][(x+w-1)%w] != 0 {
+				sum++
+			}
+			if world[(y+0)%h][(x+w+1)%w] != 0 {
+				sum++
+			}
+			if world[(y+1)%h][(x+w-1)%w] != 0 {
+				sum++
+			}
+			if world[(y+1)%h][(x+w)%w] != 0 {
+				sum++
+			}
+			if world[(y+1)%h][(x+w+1)%w] != 0 {
+				sum++
 			}
 
-			cellAlive := world[y][x] != 0
-			if cellAlive {
-				// 规则：2/3 活，其它死
-				if aliveNeighbours == 2 || aliveNeighbours == 3 {
+			// 规则（非 0 视为“当前是活”）
+			if world[y][x] != 0 {
+				if sum == 2 || sum == 3 {
 					next[y][x] = aliveVal
 				} else {
-					next[y][x] = deadVal
+					next[y][x] = 0
 				}
 			} else {
-				// 规则：死细胞若恰有 3 个活邻居则复活
-				if aliveNeighbours == 3 {
+				if sum == 3 {
 					next[y][x] = aliveVal
 				} else {
-					next[y][x] = deadVal
+					next[y][x] = 0
 				}
 			}
 		}
 	}
 
-	// 关键技巧：不改 return world，但把 world 变量重新绑定到 next
-	world = next
+	for y := 0; y < h; y++ {
+		copy(world[y], next[y])
+	}
 	return world
 }
 
 func calculateAliveCells(p golParams, world [][]byte) []cell {
-
 	h, w := p.imageHeight, p.imageWidth
 	var alive []cell
 	for y := 0; y < h; y++ {
 		for x := 0; x < w; x++ {
-			if world[y][x] != 0 { // 非 0 视为活
+			if world[y][x] != 0 { // 255 视为活
 				alive = append(alive, cell{x: x, y: y})
 			}
 		}
 	}
-
-	// ✅ 为了通过 Q4b 的测试，这里必须返回 alive
-	// 如果你坚持不改，这题的测试就注定失败
 	return alive
 }
